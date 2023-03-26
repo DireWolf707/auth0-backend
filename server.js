859ab69210b789bdd53e1868dd79f50a903385c4
DireWolf707 <rahulsood707@gmail.com>
2023-03-26 15:22:07 +0530

    refactored mongo config

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-import "./configs/dotenv/config.js"
-import "./configs/cloudinary/config.js"
-
 // Global Exception Handler
 process.on("uncaughtException", (err) => {
   console.error("UNCAUGHT EXCEPTION! Shutting down...")
@@ -8,36 +5,24 @@ process.on("uncaughtException", (err) => {
   if (process.env.NODE_ENV === "production") process.exit(1)
 })
 
+import "./configs/dotenv/config.js"
+import "./configs/mongo/config.js"
+import "./configs/cloudinary/config.js"
 import app from "./app.js"
-import mongoose from "mongoose"
 
-// Database Init
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB connection successful!")
+// Server Init
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => console.log(`App running on port ${port}...`))
 
-    // Server Init
-    const port = process.env.PORT || 3000
-    const server = app.listen(port, () => console.log(`App running on port ${port}...`))
+// Global Promise Rejection Handler
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION! Shutting down...")
+  console.error(err.name, err.message)
+  server.close(() => process.env.NODE_ENV === "production" && process.exit(1))
+})
 
-    // Global Promise Rejection Handler
-    process.on("unhandledRejection", (err) => {
-      console.error("UNHANDLED REJECTION! Shutting down...")
-      console.error(err.name, err.message)
-      server.close(() => process.env.NODE_ENV === "production" && process.exit(1))
-    })
-
-    // SIGTERM Handler
-    process.on("SIGTERM", () => {
-      console.error("SIGTERM RECEIVED! Shutting down gracefully")
-      server.close(() => console.log("Process terminated!"))
-    })
-  })
-  .catch((err) => {
-    console.error("Error in Database Connection!")
-    console.error(err.name, err.message)
-  })
+// SIGTERM Handler
+process.on("SIGTERM", () => {
+  console.error("SIGTERM RECEIVED! Shutting down gracefully")
+  server.close(() => console.log("Process terminated!"))
+})
